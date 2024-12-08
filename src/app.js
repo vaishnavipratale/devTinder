e9777c3ab8623b9a25900618e56a0dfffbe379c9
vaishnavi pratale <pratalev@gmail.com>
2024-12-08 12:14:32 +0530

    Updated feature

--- src/app.js
+++ src/app.js
@@ -1,16 +1,32 @@
 const express = require('express');
 
 const app = express();
-app.use("/test",(req,res) =>{
-    res.send("Hello from the server!!");
+
+// app.use("/hello/2",(req,res) =>{
+//     res.send("Abara ka dabra");
+// })
+
+// app.use("/hello",(req,res) =>{
+//     res.send("Hello hello  hello");
+// })
+app.get("/user",(req,res)=>{
+    res.send({firstname: "vaishnavi" , lastname : "pratale"});
 })
 
-app.use("/hello",(req,res) =>{
-    res.send("Hello hello  hello");
-})
+app.post("/user",(req,res)=>{
+    res.send("data succefully saved to the database");
+    });
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted succesfully");
+    })    
+app.use("/test",(req,res) =>{
+    res.send("Hello from the server!!");
+});
 app.use("/",(req,res) =>{
     res.send("vaishnavi pratale!");
 })
+
 
 app.listen(3000,()=>{
 console.log("server is successfully listening on port 3000");
