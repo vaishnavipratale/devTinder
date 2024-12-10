5360c2402b0bcb1263a533c2e796475d2918ddb1
vaishnavi pratale <pratalev@gmail.com>
2024-12-10 13:55:05 +0530

    Updated feature

--- src/app.js
+++ src/app.js
@@ -9,10 +9,36 @@ const app = express();
 // app.use("/hello",(req,res) =>{
 //     res.send("Hello hello  hello");
 // })
-app.get("/user/:userid/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname: "vaishnavi" , lastname : "pratale"});
-});
+
+//Dynamic routing
+// app.get("/user/:userid/:name/:password",(req,res)=>{
+//     console.log(req.params);
+//     res.send({firstname: "vaishnavi" , lastname : "pratale"});
+// });
+
+app.use("/user",(req,res, next)=>{
+    console.log("Handlling the route user 1!!");
+   
+    next();
+},
+(req,res,next)=>{
+    console.log("Handlling the route user 2!!");
+    
+    next();
+},
+(req,res,next)=>{
+    console.log("Handlling the route user 3!!");
+    next();
+},
+(req,res,next)=>{
+    console.log("Handlling the route user 4!!");
+   next();
+},
+(req,res,next)=>{
+    console.log("Handlling the route user 5!!");
+    res.send("5th response!!");
+}
+);
 
 app.listen(3000,()=>{
 console.log("server is successfully listening on port 3000");
