7cc976fae2d74332244446f2ea234a66283694ea
vaishnavi pratale <pratalev@gmail.com>
2024-12-10 20:05:36 +0530

    Updated feature

--- src/app.js
+++ src/app.js
@@ -16,30 +16,51 @@ const app = express();
 //     res.send({firstname: "vaishnavi" , lastname : "pratale"});
 // });
 
-app.use("/user",(req,res, next)=>{
-    console.log("Handlling the route user 1!!");
+//Multiple Route Handlers***************************************************
+// app.use("/user",(req,res, next)=>{
+//     console.log("Handlling the route user 1!!");
    
-    next();
-},
-(req,res,next)=>{
-    console.log("Handlling the route user 2!!");
+//     next();
+// },
+// (req,res,next)=>{
+//     console.log("Handlling the route user 2!!");
     
-    next();
-},
-(req,res,next)=>{
-    console.log("Handlling the route user 3!!");
-    next();
-},
-(req,res,next)=>{
-    console.log("Handlling the route user 4!!");
-   next();
-},
-(req,res,next)=>{
-    console.log("Handlling the route user 5!!");
-    res.send("5th response!!");
-}
-);
+//     next();
+// },
+// (req,res,next)=>{
+//     console.log("Handlling the route user 3!!");
+//     next();
+// },
+// (req,res,next)=>{
+//     console.log("Handlling the route user 4!!");
+//    next();
+// },
+// (req,res,next)=>{
+//     console.log("Handlling the route user 5!!");
+//     res.send("5th response!!");
+// }
+// );
 
+
+//Middleware example**********************************************************************
+
+//Handle Auth middleware for all GET, POST,.....request
+app.use("/admin", (req, res, next)=>{
+    console.log("admin auth is getting checked!!");
+    const token="xyz";
+    const isAdminAuthorized = token==="xyz";
+    if(!isAdminAuthorized){
+        res.status(401).send("Unauthorized request" );
+    }else{
+        next();
+    }
+});
+app.get("/admin/getAllData", (req, res,)=>{
+    res.send("User data sent");
+});
+app.get("/admin/getDeleteData", (req, res)=>{
+    res.send("User data deleted");
+});
 app.listen(3000,()=>{
 console.log("server is successfully listening on port 3000");
 });
