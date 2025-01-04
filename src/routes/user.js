dce7e2b80fc24e2d1227787c5e2342ecb5f64c55
vaishnavi pratale <pratalev@gmail.com>
2025-01-04 18:23:45 +0530

    Bilding feed API and Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const userRouter = express.Router();
+
 const {userAuth} = require("../middlewares/auth");
 const {ConnectionRequest} = require("../models/connectionRequest");
+const User  = require("../models/user");
 // Get all the pending connection request for all the loggedIn users
+
+const USER_SAFE_DATA ="firstName lastName photoUrl age gender about Skills";
 userRouter.get("/user/requests/received", userAuth, async (req, res)=>{
     try{
    const loggedInUser = req.user;
@@ -10,7 +14,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res)=>{
    const connectionRequests = await ConnectionRequest.find({
     toUserId: loggedInUser._id,
     status: "interested"
-   }).populate("fromUserId", ["firstName", "lastName", "photoUrl", "about", "gender", "Skills", ]);
+   }).populate("fromUserId", USER_SAFE_DATA);
    //   }).populate("fromUserId", "firstName lastName", ]); both work same by passing only string 
 
    res.json({
@@ -31,8 +35,8 @@ userRouter.get("/user/connections", userAuth, async (req,res) => {
             {toUserId: loggedInUser._id, status:"accepted"},
             {fromUserId:loggedInUser._id, status:"accepted"}
       ],
-      }).populate("fromUserId",["firstName", "lastName", "photoUrl", "about", "gender", "Skills", ])
-      .populate("toUserId",["firstName", "lastName", "photoUrl", "about", "gender", "Skills",]);
+      }).populate("fromUserId",USER_SAFE_DATA)
+      .populate("toUserId",USER_SAFE_DATA);
       
       console.log(connectionRequests);
      const data = connectionRequests.map((row) => {
@@ -46,5 +50,47 @@ userRouter.get("/user/connections", userAuth, async (req,res) => {
     }catch(err){
         res.status(400).send({message : err.message });
     }
+});
+
+userRouter.get("/feed", userAuth, async(req, res)=>{
+    //user should see all the user cards except 
+    //0. his own card
+    //1. his connections
+    //2. ignored people
+    //3. already sent the connection request
+
+  try{
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit>50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+ 
+
+    //find all connection requests (sent + received)
+    const connectionRequests = await ConnectionRequest.find({
+        $or:[
+            {fromUserId: loggedInUser._id}, {toUserId: loggedInUser._id}],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) =>{
+     hideUsersFromFeed.add(req.fromUserId.toString());
+     hideUsersFromFeed.add(req.toUserId.toString());
+    });
+    console.log(hideUsersFromFeed);
+
+    const users = await User.find({
+      $and:[{ _id:{ $nin: Array.from(hideUsersFromFeed)}},
+            {_id: {$ne: loggedInUser._id }},
+      ],
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+    res.json({data: users});
+
+  }catch(err){
+    res.status(400).send({message : err.message});
+  }
 });
 module.exports = { userRouter};
