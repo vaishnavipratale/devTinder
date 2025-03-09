fa7d91ae7433c88d8b84353610ef2fdb5ad64b7f
vaishnavi pratale <pratalev@gmail.com>
2025-03-09 13:06:28 +0530

    sending Emails Using SES

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -8,6 +8,6 @@ const sesClient = new SESClient({
       accessKeyId:process.env.AWS_ACCESS_KEY,
       secretAccessKey:process.env.AWS_SECRET_KEY,
     },
-  }); 
+  });
   
 module.exports= { sesClient };
